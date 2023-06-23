162145d38b1402148e100917479f33b5387fdd8d
Saturn12132 <121273872+Saturn12132@users.noreply.github.com>
2023-06-22 18:53:38 -0700

    Update Source2.lua

--- Source2.lua
+++ Source2.lua
@@ -710,7 +710,7 @@ function OrionLib:MakeWindow(WindowConfig)
 				Font = Enum.Font.GothamBold,
 				ClipsDescendants = true
 			}), "Text"),
-			AddThemeObject(SetProps(MakeElement("Label", "Using: "..Executor), {
+			AddThemeObject(SetProps(MakeElement("Label", "Using: "..Executor.."" and 14 or 13), {
 				Size = UDim2.new(1, -60, 0, 13),
 				Position = WindowConfig.HidePremium and UDim2.new(0, 50, 0, 19) or UDim2.new(0, 50, 0, 26),
 				Font = Enum.Font.GothamBold,

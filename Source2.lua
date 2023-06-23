14ed53b5b558086d1ce80706c13f7f8a1dee5e13
Saturn12132 <121273872+Saturn12132@users.noreply.github.com>
2023-06-22 19:06:40 -0700

    Update Source2.lua

--- Source2.lua
+++ Source2.lua
@@ -706,7 +706,7 @@ function OrionLib:MakeWindow(WindowConfig)
 				MakeElement("Corner", 1)
 			}),
 			AddThemeObject(SetProps(MakeElement("Label", LocalPlayer.DisplayName, WindowConfig.HidePremium and 14 or 13), {
-				Size = UDim2.new(1, -60, 0, 18),
+				Size = UDim2.new(1, -60, 0, 13),
 				Position = WindowConfig.HidePremium and UDim2.new(0, 50, 0, 19) or UDim2.new(0, 50, 0, 12),
 				Font = Enum.Font.GothamBold,
 				ClipsDescendants = true
